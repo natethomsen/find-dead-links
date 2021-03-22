181cdfdd01a554b898781b0eb30d6af513d6f752
Nathan Thomsen <natet8121@gmail.com>
2021-03-21 21:51:21 -0500

    Updated makefile, readme file, and find-dead-links

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Run this program to find the dead links of a website(NOT BVUS)
 
 ## Installing
-install python3 and virtualenv before running. After use code `make install INSTALL_DIR = <ENTER WHERE YOU WANT TO STORE PROGRAM>`
+install python3 and virtualenv before running. After use code `make install INSTALL_DIR = <ENTER WHERE YOU WANT TO STORE PROGRAM>` or type `make install` to install in the home directory. 
 
 ## Running program
 use the wrapper script to run thr program. Use code `find-dead-links <url>` or `find-dead-links <search depth> <url>`
--- find-dead-links.py
+++ find-dead-links.py
@@ -1,13 +1,83 @@
-#!/usr/bin/env
 import requests
 from bs4 import BeautifulSoup
 from sys import argv
 
-url = argv[1]
-try:
-    site = requests.get(url)
-    if r.status_code not in [200, 302]:
-        print('Bad link')
-except:
-    print('Bad link')
+def realurl(url):
+    if "http://" in url or "https://" in url:
+        return True
+    else:
+        return False
+
+def containsslash(url):
+    if url[-1] != "/":
+        url = url + "/"
+    if url[0] == "/":
+        url = url[1:]
+
+    return url
+
+def walkthrough(original):
+    original = containsslash(original)
+    #test original url for slashes
+    
+    nextlinks = []
+    #create empty list for child links
+
+    soup = BeautifulSoup(r.text, features ="html.parser")
+    
+    r=requests.get(original)
+
+    for a in soup.find_all("a", href=True):
+        nextlink = containsslash(a["href"])
+        if realurl(nextlink) == False:
+            nextlink = original + nextlink
+        nextlinks.append(nextlink)
+        #after running test add link to list
+
+    return nextlinks
+
+def testurl(urldepth, url):
+    try:
+        r = requests.get(url)
+        if r.status_code not in [200, 302]:
+            print(url, "IS A BAD LINK")
+            return
+            #if status code is not 200 or 302 it is considered bad link
+    except:
+        print(url, "IS A BAD LINK")
+        return
+            #return bad link if no status code is returned
+        if urldepth > 0:
+            nextlinks = walkthrough(url)
+            for link in nextlinks:
+                testurl(urldpeth-1, link)
+                #Run the tests on the child links of the user enters a depth
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        url = argv[1]
+        testurl(0, url)
+        #test if user does not enter a depth
+    elif len(argv) ==3:
+        urldepth = argv[1]
+        url = argv[2]
+        if urldepth.startswith("-"):
+            try:
+                urldepth = int(urldepth[1:])
+                #If url depth is entered use value after '-' as depth
+            except:
+                print("ERROR WITH DEPTH TRY AGAIN")
+                exit(1)
+                #if error is entered tell user and exit program
+            testurl(urldepth, url)
+        else:
+            print("ERROR ENTER AS FOLLOWING: find-dead-links <URL>")
+            print("ERROR ENTER AS FOLLOWING: -<depth> <URL>")
+            #Return error message if user does not enter things correctly. 
+
+
+
+
+
 
